588f8faeee3ea448aee067e40eae6c30546cbe16
ravishastri9 <ravishastri9031@gmail.com>
2021-03-02 13:43:05 +0530

    Update Bmi_Calculator_2_0.py

--- programs/Bmi_Calculator_2_0.py
+++ programs/Bmi_Calculator_2_0.py
@@ -1,3 +1,5 @@
+#Bmi (Body,Mass,Index) measures the body fat based on height and weight, by using conditionals statements
+
 height = input("enter your height in m: ")
 weight = input("enter your weight in kg: ")
 bmi = float(weight)/float(height)**2
